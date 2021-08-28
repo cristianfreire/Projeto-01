cd4d2b12a68738db87b4e7b30fe0f12dd783dde6
Walter Jonas <74218624+WALTER-OBS-DEBUG@users.noreply.github.com>
2021-08-27 23:16:09 -0400

    Update luminosidade.ino

--- Sensor de luminosidade/luminosidade.ino
+++ Sensor de luminosidade/luminosidade.ino
@@ -3,7 +3,7 @@ int valor=0; //Inicia a variável
 void setup()
 {
 	Serial.begin(9600); //Velocidade que será captado o sinal
-   	pinMode(A0, INPUT); //Pino que receberá o sinal
+   	pinMode(A0, INPUT); //Pino que recebe o sinal e envia
 }
 
 void loop()
